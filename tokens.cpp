9d191d33bcd103f9b78febec96325b466923aacd
ashwell-prince <91726052+ashwell-prince@users.noreply.github.com>
2022-01-10 01:48:49 +0530

    Created tokens.cpp
    this file include solution of 2 question 
    first and second 
    this file will print solutions keeping in mind other solution as in comment line or not selected on compiler for other particular solution of question

---
+++ tokens.cpp
@@ -0,0 +1,34 @@
+// solution for 1st question
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << " Size of char " << sizeof(char) << " bytes" <<endl;
+    cout << " Size of int " << sizeof(int) << " bytes " <<endl;
+    cout << " Size of float " << sizeof(float) << " bytes" <<endl;
+    cout << " Size of double " << sizeof(double) << " bytes" <<endl;
+
+    return 0;
+}
+
+
+
+// solution for question 2.
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    double a, b;
+
+    cout << " Enter your first number " << endl;
+    cin>>a;
+
+    cout << " Enter your second number " << endl;
+    cin>>b;
+
+    cout << " product of a and b is " << a*b << endl;
+
+    return 0; 
+}
